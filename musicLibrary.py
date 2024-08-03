fb7b689b9d9748c74a40ca576d71ae03b2065095
Lakshrajjain <lakshraj35@gmail.com>
2024-08-03 17:40:00 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "tujamo": "https://youtu.be/859u6uiZdds?list=RDQMTINwv9M3HII",
+    "jawan": "https://youtu.be/VAdGW7QDJiU?list=RDQMTINwv9M3HII",
+    "lover": "https://youtu.be/mH_LFkWxpI0?list=RDQMTINwv9M3HII",
+    "sigala": "https://youtu.be/1SJSfaa4Owo?list=RDQMTINwv9M3HII"
+}
